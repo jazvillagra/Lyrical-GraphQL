d72ad6dabad18875091df0fcde3612f2bdc50072
Jazmin Villagra <jazmin.villagra@cycleon.com>
2024-03-11 16:49:27 +0100

    Add open mongodb connection

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://jazvillagra:To82HosExreqw0jP@cluster0.bdqgcib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
