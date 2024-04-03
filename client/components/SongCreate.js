4941a571a04e1b8061648c099c1f1733a7994513
Jazmin Villagra <jazmin.villagra@cycleon.com>
2024-04-03 12:27:31 +0200

    Add SongCreate component with mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 class SongCreate extends Component {
   constructor(props) {
     super(props);
     this.state = { title: "" };
   }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      },
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>Create a new song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song title:</label>
           <input
             onChange={(event) => this.setState({ title: event.target.value })}
@@ -20,5 +32,26 @@ class SongCreate extends Component {
     );
   }
 }
+/*
+GraphiQL mutation:
+mutation AddSong($songTitle: String) {
+  addSong(title: $songTitle) {
+    id
+    title
+  }
+}
 
-export default SongCreate;
+Query variables:
+{"songTitle": "Seven"}
+*/
+
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
+//gives us access to props.object
+export default graphql(mutation)(SongCreate);
