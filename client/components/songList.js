ded6cbd9ad304fb3370eb8f6708ed88f0e35bdf4
Jazmin Villagra <jazmin.villagra@cycleon.com>
2024-04-03 11:37:35 +0200

    Reformat with eslint

--- client/components/songList.js
+++ client/components/songList.js
@@ -1,37 +1,33 @@
 import React, { Component } from "react";
-import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
-class SongList extends Component{
-    renderSongs() {
-        return this.props.data.songs.map(song => {
-            return(
-                <li key={song.id} className="collection-item">
-                    {song.title}
-                </li>
-            )
-        });
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map((song) => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    });
+  }
+  render() {
+    //for async handling (temporary)
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
     }
-    render() {
-        //for async handling (temporary)
-        if (this.props.data.loading) {
-            return <div>Loading...</div>;
-        }
-        
-        return (
-        <ul className="collection">
-            {this.renderSongs()}
-        </ul>
-        );
-    }
+
+    return <ul className="collection">{this.renderSongs()}</ul>;
+  }
 }
 const query = gql`
-    {
-        songs {
-            id
-            title
-        }
+  {
+    songs {
+      id
+      title
     }
+  }
 `;
 //first parenthesis content returns a function that is called by the second set of parenthesis
 export default graphql(query)(SongList);
