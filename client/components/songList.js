0deeb16fb86318834e41722b24936fcb32a33660
Jazmin Villagra <jazmin.villagra@cycleon.com>
2024-04-03 11:15:53 +0200

    Fix key issues

--- client/components/songList.js
+++ client/components/songList.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component{
+    renderSongs() {
+        return this.props.data.songs.map(song => {
+            return(
+                <li key={song.id} className="collection-item">
+                    {song.title}
+                </li>
+            )
+        });
+    }
     render() {
-        return (<div>
-            SongList
-        </div>
+        //for async handling (temporary)
+        if (this.props.data.loading) {
+            return <div>Loading...</div>;
+        }
+        
+        return (
+        <ul className="collection">
+            {this.renderSongs()}
+        </ul>
         );
     }
 }
-
-export default SongList;
+const query = gql`
+    {
+        songs {
+            id
+            title
+        }
+    }
+`;
+//first parenthesis content returns a function that is called by the second set of parenthesis
+export default graphql(query)(SongList);
